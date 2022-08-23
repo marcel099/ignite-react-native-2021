bf9ca5bc72dfe4082b085056512447ad1178ff14
Marcelo Lupatini <mar.lupatini@gmail.com>
2022-08-23 15:55:09 -0300

    Cap. 3, M6 #3 - S1, Cabeçalho da tela Home

--- 03-rentx/src/screens/Home/index.tsx
+++ 03-rentx/src/screens/Home/index.tsx
@@ -1,12 +1,36 @@
+import { StatusBar } from "react-native";
+import { RFValue } from "react-native-responsive-fontsize";
+
+import LogoSvg from '../../assets/logo.svg';
+
 import {
   Container,
-  Title,
+  Header,
+  HeaderContent,
+  TotalCars,
 } from "./styles";
 
 export function Home() {
   return (
-    <Container>
-      <Title>Home</Title>
-    </Container>
+    <>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      <Container>
+        <Header>
+          <HeaderContent>
+            <LogoSvg
+              width={RFValue(108)}
+              height={RFValue(12)}
+            />
+            <TotalCars>
+              Total de 12 carros
+            </TotalCars>
+          </HeaderContent>
+        </Header>
+      </Container>
+    </>
   );
 }
--- 03-rentx/src/screens/Home/styles.ts
+++ 03-rentx/src/screens/Home/styles.ts
@@ -1,14 +1,30 @@
+import { RFValue } from "react-native-responsive-fontsize";
 import styled from "styled-components/native";
 
 export const Container = styled.View`
   flex: 1; 
-  justify-content: center;
-  align-items: center;
 
   background-color: ${({ theme }) => theme.colors.background_primary};
 `;
 
-export const Title = styled.Text`
-  font-size: 30px;
-  font-family: ${({ theme }) => theme.fonts.secondary_500};
+export const Header = styled.View` 
+  width: 100%;
+  height: ${RFValue(113)}px;
+
+  background-color: ${({ theme }) => theme.colors.header};
+  padding: 0 ${RFValue(16)}px ${RFValue(28)}px ${RFValue(16)}px;
+`;
+
+export const HeaderContent = styled.View`
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+
+  margin-top: auto;
+`;
+
+export const TotalCars = styled.Text`
+  font-size: ${RFValue(15)};
+  font-family: ${({ theme }) => theme.fonts.primary_400};
+  color: ${({ theme }) => theme.colors.text};
 `;
