bc16f8b64c9ce88eb851747741ba55bb6af98622
Marcelo Lupatini <mar.lupatini@gmail.com>
2022-08-23 13:56:45 -0300

    Cap. 3, M6 #1 - S1, Criação do projeto Rentx

---
+++ 03-rentx/App.tsx
@@ -0,0 +1,7 @@
+import { Home } from "./src/screens/Home";
+
+export function App() {
+  return (
+    <Home />
+  );
+}
---
+++ 03-rentx/src/screens/Home/index.tsx
@@ -0,0 +1,12 @@
+import {
+  Container,
+  Title,
+} from "./styles";
+
+export function Home() {
+  return (
+    <Container>
+      <Title>Home</Title>
+    </Container>
+  );
+}
