4c4d311e2144f0aed98699f93b985d86261e6ae6
Marcelo Lupatini <mar.lupatini@gmail.com>
2022-08-23 14:41:30 -0300

    Cap. 3, M6 #2 - S1, Tema global

--- 03-rentx/App.tsx
+++ 03-rentx/App.tsx
@@ -1,7 +1,34 @@
+import { useFonts } from "expo-font";
+import {
+  Inter_400Regular,
+  Inter_500Medium,
+} from "@expo-google-fonts/inter";
+import {
+  Archivo_400Regular,
+  Archivo_500Medium,
+  Archivo_600SemiBold,
+} from "@expo-google-fonts/archivo";
+import { ThemeProvider } from "styled-components/native";
+
+import { theme } from "./src/global/styles/theme"
 import { Home } from "./src/screens/Home";
 
 export function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium,
+    Archivo_400Regular,
+    Archivo_500Medium,
+    Archivo_600SemiBold,
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
-    <Home />
+    <ThemeProvider theme={theme}>
+      <Home />
+    </ThemeProvider>
   );
 }
