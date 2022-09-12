df534e82772a8265654c297d1524d809790c4481
Marcelo Lupatini <mar.lupatini@gmail.com>
2022-09-12 19:09:11 -0300

    Cap. 5, M9 #9 - S3, Config. falha para testes E2E

--- 01-myskills/src/pages/Home/index.tsx
+++ 01-myskills/src/pages/Home/index.tsx
@@ -47,7 +47,7 @@ export function Home() {
 
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>
+      <Text style={styles.title} testID="welcome">
         Welcome, Rodrigo
       </Text>
       <Text style={styles.greeting}>
