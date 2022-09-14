7a916ba3d8070fad9252e416c929eaca841cfaaf
Marcelo Lupatini <mar.lupatini@gmail.com>
2022-09-13 22:27:44 -0300

    Cap. 6, M11 #3 - S2, Add. SplashScreen do MySkills

--- 01-myskills/App.tsx
+++ 01-myskills/App.tsx
@@ -1,11 +1,14 @@
-import React from 'react';
-
+import React, { useEffect } from 'react';
 import { StatusBar } from 'react-native';
-// import { Text } from 'react-native';
+import SplashScreen from 'react-native-splash-screen';
 
 import { Home } from './src/pages/Home';
 
 export default function App() {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
   return (
     <>
       {/* <Text>Algum texto</Text> */}
