ae1b226b720d700c275ab695f65d6ad13f453798
Marcelo Lupatini <mar.lupatini@gmail.com>
2022-09-15 21:25:13 -0300

    MFB, #4 - S3, Integrando app com Cloud Storage

--- bonus/myshopping/src/screens/Upload/index.tsx
+++ bonus/myshopping/src/screens/Upload/index.tsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
+import { Alert } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
+import storage from '@react-native-firebase/storage';
 
 import { Button } from '../../components/Button';
 import { Header } from '../../components/Header';
@@ -9,6 +11,8 @@ import { Container, Content, Progress, Transferred } from './styles';
 
 export function Upload() {
   const [image, setImage] = useState('');
+  const [transferredBytes, setTransferredBytes] = useState('');
+  const [progress, setProgress] = useState('0');
 
   async function handlePickImage() {
     const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
@@ -26,24 +30,47 @@ export function Upload() {
     }
   };
 
+  async function handleUpload() {
+    const fileName = new Date().getTime();
+    const MIME = image.match(/\.(?:.(?!\.))+$/)
+    const reference = storage().ref(`/images/${fileName}${MIME}`);
+
+    const uploadTask = reference.putFile(image);
+
+    uploadTask.on('state_changed', taskSnapshot => {
+      const percent = ((taskSnapshot.bytesTransferred / taskSnapshot.totalBytes) * 100).toFixed(0);
+
+      setProgress(percent);
+      setTransferredBytes(
+        `${taskSnapshot.bytesTransferred} transferidos de ${taskSnapshot.totalBytes}`
+      )
+    });
+
+    uploadTask.then(async () => {
+      const imageUrl = await reference.getDownloadURL();
+      console.log(imageUrl)
+      Alert.alert('Upload concluído com sucesso!');
+    })
+  }
+
   return (
     <Container>
-      <Header title="Lista de compras" />
+      <Header title="Upload de Fotos" />
 
       <Content>
         <Photo uri={image} onPress={handlePickImage} />
 
         <Button
           title="Fazer upload"
-          onPress={() => { }}
+          onPress={handleUpload}
         />
 
         <Progress>
-          0%
+          {progress}%
         </Progress>
 
         <Transferred>
-          0 de 100 bytes transferido
+          {transferredBytes} de 100 bytes transferido
         </Transferred>
       </Content>
     </Container>
